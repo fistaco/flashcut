e8d0bde6e2fa52165df7a50620b8bd189a31c3cb
fistaco <fiske_schijlen@hotmail.com>
2022-05-28 18:11:04 +0200

    Don't require a video file to construct a FlashCut instance.

--- flashcut.py
+++ flashcut.py
@@ -3,11 +3,14 @@ import librosa
 
 
 class FlashCut():
-    def __init__(self, video_filepath) -> None:
-        self.cap = cv2.VideoCapture(video_filepath)
+    def __init__(self, video_filepath=None) -> None:
+        self.cap = None
+        if video_filepath is not None:
+            self.cap = cv2.VideoCapture(video_filepath)
 
     def __del__(self):
-        self.cap.release()
+        if self.cap is not None:
+            self.cap.release()
         cv2.destroyAllWindows()
 
     def run(self):
@@ -22,9 +25,24 @@ class FlashCut():
             # Preprocess image
             frame = self.preprocess_frame(frame, 960, 540)
 
+    def is_flashback_frame(self, frame):
+        """
+        Returns whether the given `frame` is part of a flashback based on its
+        colours.
+        """
+        pass
+
+    def is_flashback_transition_frame(self, frame):
+        """
+        Returns whether the given `frame` is part of a transition to a
+        flashback based on its colours, brightness, and blur.
+        """
+        pass
+
     def preprocess_frame(self, frame, width, height):
         """
-        Preprocesses the given `frame` by reducing its dimensions to
+        Preprocesses the given `frame` by reducing its dimensions to the given
+        `width` and `height`.
         """
         return cv2.resize(frame, (width, height))
 
@@ -38,5 +56,5 @@ class FlashCut():
         """
         Displays the given image until any key is pressed.
         """
-        cv2.imshow(img)
+        cv2.imshow(winname="Image", mat=img)
         cv2.waitKey(0)
